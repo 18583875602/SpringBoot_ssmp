f2238485d570d9c3e4e35c3b6e9d049ea4b9cd9c
zhiYuPan <zhiyupanoutlook@163.com>
2023-06-01 20:06:22 +0800

    hot-fix commit2

--- SpringBoot_ssmp/src/main/java/com/pan/SSMPApplication.java
+++ SpringBoot_ssmp/src/main/java/com/pan/SSMPApplication.java
@@ -8,6 +8,7 @@ public class SSMPApplication {
 
     public static void main(String[] args) {
         System.out.println("this is hot fix----------");
+        System.out.println("this is hot fix2----------");
         SpringApplication.run(SSMPApplication.class, args);
     }
 
